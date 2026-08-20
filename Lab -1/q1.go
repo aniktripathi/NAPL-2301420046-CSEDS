0639f1634c8158d0e7f8c868d953ddc6a4f0067e
aniktripathi27 <aniktripathi27@gmail.com>
2026-08-20 10:52:43 +0530

    assignment -1

---
+++ Lab -1/q1.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, anik")
+	fmt.Println("Interger Operations")
+
+	var a int = 20
+	var b int = 10
+
+	var x float64 = 20.5
+	var y float64 = 10.5
+
+	fmt.Println("Integer Addition:", a+b)
+	fmt.Println("Integer Subtraction:", a-b)
+	fmt.Println("Integer Multiplication:", a*b)
+	fmt.Println("Integer Division:", a/b)
+
+	// folat operations
+	fmt.Println("Float Addition:", x+y)
+	fmt.Println("Float Subtraction:", x-y)
+	fmt.Println("Float Multiplication:", x*y)
+	fmt.Println("Float Division:", x/y)
+}
