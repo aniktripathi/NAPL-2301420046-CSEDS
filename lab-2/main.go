a9194d5c2461a2efb27633b1440c61c3d41959e6
aniktripathi27 <aniktripathi27@gmail.com>
2026-09-01 12:17:25 +0530

    Add lab-2

---
+++ lab-2/main.go
@@ -0,0 +1,12 @@
+package main
+import "example.com/app/mathutil"
+
+import "fmt"
+
+func main() {
+	result := mathutil.Add(5, 10)
+	fmt.Println("The sum is:", result)
+}
+
+
+
---
+++ lab-2/mathutil/add.go
@@ -0,0 +1,7 @@
+package mathutil
+
+func Add(a, b int) int {
+	return a + b
+}
+// main.go
+//mathutil.Add(5, 10)
