a32d274345b30f3cb1a506ed298de38d47e12c49
aniktripathi27 <aniktripathi27@gmail.com>
2026-09-15 11:47:11 +0530

    Add lab 2 q2 Go source

---
+++ lab-2/q2.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() {
+
+
+	students := []string{"Anik", "Rahul", "Priya"}
+
+	fmt.Println("Initial Slice:", students)
+
+	// ADD
+	students = append(students, "Aman")
+	fmt.Println("After ADD:", students)
+
+	index := 1
+	students = append(students[:index], students[index+1:]...)
+	fmt.Println("After REMOVE at index 1:", students)
+
+	// UPDATe
+	students[1] = "Neha"
+	fmt.Println("After UPDATE at index 1:", students)
+
+	fmt.Println("\n===== MAP OPERATIONS =====")
+
+	marks := map[string]int{
+		"Maths":    85,
+		"Science":  90,
+		"English":  78,
+	}
+
+	fmt.Println("Initial Map:", marks)
+
+	
+	marks["Computer"] = 95
+	fmt.Println("After INSERT:", marks)
+
+	
+	delete(marks, "English")
+	fmt.Println("After DELETE:", marks)
+
+	
+	subject := "Maths"
+	mark, exists := marks[subject]
+
+	if exists {
+		fmt.Println("LOOKUP:", subject, "=", mark)
+	} else {
+		fmt.Println("LOOKUP:", subject, "not found")
+	}
+
+	fmt.Println("Map After LOOKUP:", marks)
+}
